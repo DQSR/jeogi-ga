cd498bc84fbc21fdc7bc3a159de3fac30349f9f6
jwp0116 <cong0116@naver.com>
2020-05-23 20:29:43 +0900

    change PORT env

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app
 
 app.use(require('koa-static')('public'));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 console.log('Server is Running')
